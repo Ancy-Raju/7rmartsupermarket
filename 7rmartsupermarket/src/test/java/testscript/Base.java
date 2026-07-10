923ed5438b74a9e0c9ac3353f0bb182d027e1c1b
jolly <jolly@192.168.0.3>
2026-07-10 11:54:25 +0530

    fifth push

--- 7rmartsupermarket/src/test/java/testscript/Base.java
+++ 7rmartsupermarket/src/test/java/testscript/Base.java
@@ -1,15 +1,18 @@
 package testscript;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 //import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import utilities.ScreenShotUtility;
 import utilities.WaitUtility;
 
 public class Base {
@@ -35,10 +38,15 @@ public class Base {
 	}
 
 	@AfterMethod(alwaysRun = true)
-	public void closeBrowser() {
-		driver.close();
+	public void browserQuit(ITestResult iTestResult) throws IOException {
+		if (iTestResult.getStatus() == ITestResult.FAILURE) {
+			ScreenShotUtility scrShot = new ScreenShotUtility(); // creating obj
+			scrShot.getScreenShot(driver, iTestResult.getName());
+			
+		}
 	}
 }
+
 
 
 
