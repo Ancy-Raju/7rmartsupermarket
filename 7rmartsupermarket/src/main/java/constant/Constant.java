0a24a6c2b4cb84304683ec09767985bfb1ab0df7
jolly <jolly@192.168.0.4>
2026-07-16 13:11:06 +0530

    seventh push

--- 7rmartsupermarket/src/main/java/constant/Constant.java
+++ 7rmartsupermarket/src/main/java/constant/Constant.java
@@ -4,5 +4,5 @@ public class Constant {
 
 	public static final String TESTDATA = System.getProperty("user.dir") + "\\src\\test\\resources\\Test Data1.xlsx";
 	public static final String TOOTHPATEIMG = System.getProperty("user.dir") + "\\src\\test\\resources\\toothpate.jpg";
-
+	 public static final String CONFIGFILE=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
 }
--- 7rmartsupermarket/src/test/java/testscript/Base.java
+++ 7rmartsupermarket/src/test/java/testscript/Base.java
@@ -1,7 +1,9 @@
 package testscript;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,39 +14,54 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import constant.Constant;
 import utilities.ScreenShotUtility;
 import utilities.WaitUtility;
 
 public class Base {
 
 	public WebDriver driver;
-
-	@BeforeMethod(alwaysRun = true)
-
+	Properties pr;
+	FileInputStream fileinput;
+	@BeforeMethod
 	@Parameters("browser")
-	public void initializeBrowser(String browser) throws Exception {
-		if (browser.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();
-		} else if (browser.equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
-
-		} else {
-			throw new Exception("invalid browser");
+	public void initializebrowser(String Browser) throws Exception
+	{
+		try
+		{
+			pr=new Properties();
+			fileinput=new FileInputStream(Constant.CONFIGFILE);
+			pr.load(fileinput);
 		}
-		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		if(Browser.equalsIgnoreCase("Chrome"))
+		{
+		driver=new ChromeDriver();
+		}
+		else if(Browser.equalsIgnoreCase("Firefox"))
+		{
+			driver=new FirefoxDriver();
+		}
+		else
+		{
+			throw new Exception("Invalid Browser");
+		}
+		//driver.get("https://groceryapp.uniqassosiates.com/admin/login");
+		driver.get(pr.getProperty("url"));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitUtility.IMPLICITWAIT));
 		driver.manage().window().maximize();
-
+		
 	}
-
-	@AfterMethod(alwaysRun = true)
+	@AfterMethod
 	public void browserQuit(ITestResult iTestResult) throws IOException {
 		if (iTestResult.getStatus() == ITestResult.FAILURE) {
 			ScreenShotUtility scrShot = new ScreenShotUtility(); // creating obj
 			scrShot.getScreenShot(driver, iTestResult.getName());
-			
 		}
-		driver.quit();
+  driver.quit();
 	}
 }
 
